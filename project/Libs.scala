a15903683133941c1de83e17ef480b14061d3e02
Pritam Kadam <phkadam2008@gmail.com>
2020-05-22 16:59:45 +0530

    Upgrade esw

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,11 +3,11 @@ import sbt._
 object Libs {
   val ScalaVersion = "2.13.2"
 
-  val `scalatest` = "org.scalatest" %% "scalatest" % "3.1.1" //Apache License 2.0
+  val `scalatest` = "org.scalatest" %% "scalatest" % "3.1.2" //Apache License 2.0
 }
 
 object ESW {
-  val Version: String = "509a40d6a2"
+  val Version: String = "2d5ad1b"
 
   val `esw-testkit` = "com.github.tmtsoftware.esw" %% "esw-testkit" % Version
 }
