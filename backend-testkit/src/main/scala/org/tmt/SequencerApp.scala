3f9a73f675a5635ce9006f1240b39bd7895593b3
ManishGowardipe <gowardipemanish95@gmail.com>
2020-05-27 17:05:03 +0530

    #000: Manish | Chauhan : Add functionality to start sequencer
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

---
+++ backend-testkit/src/main/scala/org/tmt/SequencerApp.scala
@@ -0,0 +1,17 @@
+package org.tmt
+
+import caseapp.RemainingArgs
+import csw.prefix.models.Subsystem
+import esw.http.core.commons.EswCommandApp
+import esw.ocs.testkit.EswTestKit
+import org.tmt.TSSequencerCommands._
+
+object SequencerApp extends EswCommandApp[TSSequencerCommands] {
+  private lazy val eswTestKit = new EswTestKit() {}
+
+  override def run(options: TSSequencerCommands, remainingArgs: RemainingArgs): Unit =
+    options match {
+      case Start(subsystem: Subsystem, observingMode: String) =>
+        eswTestKit.spawnSequencerInSimulation(subsystem, observingMode)
+    }
+}
