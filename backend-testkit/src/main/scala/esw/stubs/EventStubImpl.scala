a60336a3242e9bb053640912950015e589a95dcc
Himanshu Mertia <hmertia@thoughtworks.com>
2020-08-05 09:16:47 +0530

    #ESW-318: Himanshu : Modify event stub to use data from request

--- backend-testkit/src/main/scala/esw/stubs/EventStubImpl.scala
+++ backend-testkit/src/main/scala/esw/stubs/EventStubImpl.scala
@@ -3,12 +3,10 @@ package esw.stubs
 import akka.Done
 import akka.actor.typed.{ActorSystem, SpawnProtocol}
 import akka.stream.scaladsl.Source
-import csw.params.core.models.Id
 import csw.params.events.{Event, EventKey, EventName, ObserveEvent}
 import csw.prefix.models.Subsystem.CSW
 import csw.prefix.models.{Prefix, Subsystem}
 import esw.gateway.api.EventApi
-import esw.gateway.api.protocol.{EmptyEventKeys, EventServerUnavailable, InvalidMaxFrequency}
 import esw.gateway.impl.SourceExtensions.RichSource
 import esw.ocs.testkit.utils.BaseTestSuite
 import msocket.api.Subscription
@@ -18,43 +16,35 @@ import scala.concurrent.duration.DurationLong
 
 class EventStubImpl(_actorSystem: ActorSystem[SpawnProtocol.Command]) extends EventApi with BaseTestSuite {
 
-  lazy val validObserveEvent: Event = ObserveEvent(Prefix(CSW, "ncc.trombone"), EventName("offline"))
+  var events: Set[Event] = Set.empty
 
   implicit def actorSystem: ActorSystem[SpawnProtocol.Command] = _actorSystem
 
   override def publish(event: Event): Future[Done] = {
-    if (event.eventId == Id("InvalidEvent")) Future.failed(new EventServerUnavailable)
+    events += event
     Future.successful(Done)
   }
 
   override def get(eventKeys: Set[EventKey]): Future[Set[Event]] = {
-    if (eventKeys.nonEmpty) {
-      Future.successful(Set(validObserveEvent))
-    }
-    else Future.failed(new EmptyEventKeys)
+    Future.successful(events.filter(e => eventKeys.contains(e.eventKey)))
   }
 
   override def subscribe(eventKeys: Set[EventKey], maxFrequency: Option[Int]): Source[Event, Subscription] = {
-    val stream =
-      if (eventKeys.nonEmpty) validResponseStream
-      else if (maxFrequency.getOrElse(0) <= 0) Source.failed(new InvalidMaxFrequency)
-      else Source.failed(new EmptyEventKeys)
-
-    stream.withSubscription()
+    {
+      val futureStream = future(2.seconds, Source(events.filter(e => eventKeys.contains(e.eventKey))))
+      Source
+        .futureSource(futureStream)
+        .mapMaterializedValue(_ => () => ())
+    }.withSubscription()
   }
 
   override def pSubscribe(subsystem: Subsystem, maxFrequency: Option[Int], pattern: String): Source[Event, Subscription] = {
-    val stream =
-      if (maxFrequency.getOrElse(0) <= 0) Source.failed(new InvalidMaxFrequency)
-      else validResponseStream
-
-    stream.withSubscription()
+    {
+      val futureStream = future(2.seconds, Source(events.filter(e => e.source.subsystem == subsystem)))
+      Source
+        .futureSource(futureStream)
+        .mapMaterializedValue(_ => () => ())
+    }.withSubscription()
   }
 
-  private def validResponseStream: Source[Event, () => Unit] = {
-    val futureStream = future(2.seconds, Source(List(validObserveEvent)))
-    Source
-      .futureSource(futureStream)
-      .mapMaterializedValue(_ => () => ())
-  }
 }
